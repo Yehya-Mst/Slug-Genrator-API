e1083f07a99f6ae7158fa507de84a6d3d0cecc78
Yehya <yehyam661@gmail.com>
2026-05-08 20:29:53 +0300

    slug service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Injectable, Module } from '@nestjs/common';
 import { SlugService } from './slug.service.js';
 import { SlugController } from './slug.controller.js';
-
+import { PrismaModule } from './prisma/prisma.module.js';
+import { PrismaService } from './prisma/prisma.service.js';
 @Module({
+  imports: [PrismaModule],
   controllers: [SlugController],
-  providers: [SlugService],
+  providers: [SlugService, PrismaService],
 })
 export class AppModule {}
